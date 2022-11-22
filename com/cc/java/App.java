ea4dae52ba4fe8bb4828eebad86cecc66c9ac859
Peter <schmidt.multimedia@gmail.com>
2022-11-22 13:04:34 +0100

    Referenzvariable  /  Instanzvariable

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,14 +3,36 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        /** Typkonvertierung  */
+        App.output(String.valueOf(2));  // int --> String
+        App.output(Integer.toString(2));  // int --> String
+        App.output("Das ist eine Ziffer: " + 2);  // int --> String
+
+        output("------------------------");
+        
+        // neues Objekt wird erzeugt, Adresse wird cat zugewiesen
+        Cat cat = new Cat();
+        // Methode output befindet sich in der KLASSE App
+        App.output("Blick von aussen: "+ cat); 
+        // Methode tellYourAddress() befindet sich im OBJEKT cat 
+        cat.tellYourAddress();
+    
+        output("------------------------");
+
+        // neues Objekt wird erzeugt, Adresse wird cat1 zugewiesen
+        Cat cat1 = new Cat();
+        // Methode output befindet sich in der KLASSE App
+        output("Blick von aussen: "+ cat1); 
+        // Methode tellYourAddress() befindet sich im OBJEKT cat1 
+        cat1.tellYourAddress();
+
     }
 
+    // Methode witrd aus der KLASSE aufgerufen, daher static
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
-
 
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,16 @@
+package com.cc.java;
+
+public class Cat {
+    
+public void tellYourAddress(){
+   // Aufruf der Methode output() in der KLASSE App
+   // this ist die Referenz des Objektes auf sich selbst
+    App.output("Blick von innen: " + this);
+}
+
+
+
+
+
+    
+}
