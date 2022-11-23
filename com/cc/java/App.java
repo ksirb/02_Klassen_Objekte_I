ba02dd941d958b624b3ee14ff02588841136f5b7
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-23 11:53:44 +0100

    private / getter / setter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -12,29 +12,29 @@ public class App {
         // output("------------------------");
         
         // neues Objekt wird erzeugt, Adresse wird cat zugewiesen
-        Cat cat = new Cat();
-        cat.name = "Grizabella";
-        cat.furColor = "White";
-        cat.age = 29;
+        Cat cat = new Cat("Grizabella", "White", 29);
+        // cat.name = "Grizabella";
+        // cat.furColor = "White";
+        // cat.age = 29;
 
-        // output(cat.name);
-        // output(cat.furColor);
-        // output(String.valueOf(cat.age));
+        
 
-        output(cat.tellYourName());
-        output(cat.tellYourFurColor());
-        output(String.valueOf(cat.tellYourAge()));
+        output(cat.getName());
+        output(cat.getFurColor());
+        output(String.valueOf(cat.getAge()));
 
         output("------------------------");
 
-        Cat cat1 = new Cat();
-        cat1.name = "Alonzo";
-        cat1.furColor = "Grey";
-        cat1.age = 35;
+        Cat cat1 = new Cat("Alonzo", "Grey", 35);
+        // cat1.name = "Alonzo";
+        // cat1.furColor = "Grey";
+        // cat1.age = 35;
 
-        output(cat1.tellYourName());
-        output(cat1.tellYourFurColor());
-        output(String.valueOf(cat1.tellYourAge()));
+        cat1.setFurColor("Striped");;
+
+        output(cat1.getName());
+        output(cat1.getFurColor());
+        output(String.valueOf(cat1.getAge()));
 
         // // Methode output befindet sich in der KLASSE App
         // App.output("Blick von aussen: "+ cat); 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,21 +2,57 @@ package com.cc.java;
 
 public class Cat {
 
-    public String name;
-    public String furColor;
-    public int age;
+    private String name;
+    private String furColor;
+    private int age;
 
-    public String tellYourName() {
-        return this.name;
-    }
+        public Cat(String name, String furColor, int age) {
+            this.name = name;
+            this.furColor = furColor;
+            this.age = age;
+        }
 
-    public String tellYourFurColor() {
-        return this.furColor;
-    }
+        
 
-    public int tellYourAge() {
-        return this.age;
-    }
+        public String getName() {
+            return name;
+        }
+
+
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+
+        public String getFurColor() {
+            return furColor;
+        }
+
+
+
+        public void setFurColor(String furColor) {
+            this.furColor = furColor;
+        }
+
+
+
+        public int getAge() {
+            return age;
+        }
+
+
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+
+
+        public void changeName(String name) {
+            this.name = name;
+        }
     
 public void tellYourAddress(){
    // Aufruf der Methode output() in der KLASSE App
