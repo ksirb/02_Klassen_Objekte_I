a0dbd52eff21fec40c7aae7611073a09d0c90b8b
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-23 10:07:50 +0100

    Felder / Methoden

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,28 +4,51 @@ public class App {
     
     public static void main(String[] args) {
 
-        /** Typkonvertierung  */
-        App.output(String.valueOf(2));  // int --> String
-        App.output(Integer.toString(2));  // int --> String
-        App.output("Das ist eine Ziffer: " + 2);  // int --> String
+        // /** Typkonvertierung  */
+        // App.output(String.valueOf(2));  // int --> String
+        // App.output(Integer.toString(2));  // int --> String
+        // App.output("Das ist eine Ziffer: " + 2);  // int --> String
 
-        output("------------------------");
+        // output("------------------------");
         
         // neues Objekt wird erzeugt, Adresse wird cat zugewiesen
         Cat cat = new Cat();
-        // Methode output befindet sich in der KLASSE App
-        App.output("Blick von aussen: "+ cat); 
-        // Methode tellYourAddress() befindet sich im OBJEKT cat 
-        cat.tellYourAddress();
-    
+        cat.name = "Grizabella";
+        cat.furColor = "White";
+        cat.age = 29;
+
+        // output(cat.name);
+        // output(cat.furColor);
+        // output(String.valueOf(cat.age));
+
+        output(cat.tellYourName());
+        output(cat.tellYourFurColor());
+        output(String.valueOf(cat.tellYourAge()));
+
         output("------------------------");
 
-        // neues Objekt wird erzeugt, Adresse wird cat1 zugewiesen
         Cat cat1 = new Cat();
-        // Methode output befindet sich in der KLASSE App
-        output("Blick von aussen: "+ cat1); 
-        // Methode tellYourAddress() befindet sich im OBJEKT cat1 
-        cat1.tellYourAddress();
+        cat1.name = "Alonzo";
+        cat1.furColor = "Grey";
+        cat1.age = 35;
+
+        output(cat1.tellYourName());
+        output(cat1.tellYourFurColor());
+        output(String.valueOf(cat1.tellYourAge()));
+
+        // // Methode output befindet sich in der KLASSE App
+        // App.output("Blick von aussen: "+ cat); 
+        // // Methode tellYourAddress() befindet sich im OBJEKT cat 
+        // cat.tellYourAddress();
+    
+        // output("------------------------");
+
+        // // neues Objekt wird erzeugt, Adresse wird cat1 zugewiesen
+        // Cat cat1 = new Cat();
+        // // Methode output befindet sich in der KLASSE App
+        // output("Blick von aussen: "+ cat1); 
+        // // Methode tellYourAddress() befindet sich im OBJEKT cat1 
+        // cat1.tellYourAddress();
 
     }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,6 +1,22 @@
 package com.cc.java;
 
 public class Cat {
+
+    public String name;
+    public String furColor;
+    public int age;
+
+    public String tellYourName() {
+        return this.name;
+    }
+
+    public String tellYourFurColor() {
+        return this.furColor;
+    }
+
+    public int tellYourAge() {
+        return this.age;
+    }
     
 public void tellYourAddress(){
    // Aufruf der Methode output() in der KLASSE App
